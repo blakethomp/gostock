68fe79a203ffd8c89018dd259fe3b26344d22b44
Blake Thompson <bthompson@mmgyglobal.com>
2015-02-26 14:58:06 -0600

    Revert "just clear it"
    This reverts commit 4e6f3e6faf61d5e865600022af5f794dc78cc1a5.

--- gostock.go
+++ gostock.go
@@ -34,6 +34,8 @@ type Data struct {
 	Pct string `xml:"changePct"`
 }
 
+var clear bool
+
 var intervalFlag time.Duration
 
 func init() {
@@ -42,6 +44,7 @@ func init() {
 }
 
 func main() {
+	clear = false
 
 	flag.Parse()
 
@@ -114,9 +117,13 @@ func formatOutput (s Stock) {
 
 	w.Init(os.Stdout, 0, 8, 1, '\t', 0)
 
-	// Clear the terminal and reset the cursor
-	fmt.Print("\033[2J\033[H")
-
+	if clear {
+		fmt.Fprintf(w, "\033[%dA", len(s.Data) + 2)
+	} else {
+		// Clear the terminal and reset the cursor
+		fmt.Print("\033[2J\033[H")
+		clear = true
+	}
 
 	fmt.Fprintln(w, "\033[K" + time.Now().Round(time.Second).String())
 
